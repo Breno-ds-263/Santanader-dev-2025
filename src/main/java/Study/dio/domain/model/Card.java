4f291314bafe467f8e37696d1dbe8fc0e80efbdd
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-20 14:46:34 -0300

    Feat<Card>: add model Card

---
+++ src/main/java/Study/dio/domain/model/Card.java
@@ -0,0 +1,30 @@
+package Study.dio.domain.model;
+
+public class Card {
+	private String number;
+	private Double limit;
+	
+	public Card() {
+		super();
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public Double getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Double limit) {
+		this.limit = limit;
+	}
+	
+	
+	
+
+}
