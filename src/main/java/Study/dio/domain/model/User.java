e2e9a82756c5e1a6ab1567223ee563f39ec7c82f
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-20 14:48:51 -0300

    Feat<User>: add model User

---
+++ src/main/java/Study/dio/domain/model/User.java
@@ -0,0 +1,59 @@
+package Study.dio.domain.model;
+
+import java.util.List;
+
+public class User {
+	private String name;
+	private Account account;
+	private List<Feature> feature;
+	private Card card;
+	private List<News> news;
+	
+	public User() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public List<Feature> getFeature() {
+		return feature;
+	}
+
+	public void setFeature(List<Feature> feature) {
+		this.feature = feature;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	public List<News> getNews() {
+		return news;
+	}
+
+	public void setNews(List<News> news) {
+		this.news = news;
+	}
+	
+	
+	
+
+}
