df3dc40e771c74acc8b321ecfd80ae8199c5a980
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-20 14:47:52 -0300

    Feat<News>: add model News

---
+++ src/main/java/Study/dio/domain/model/News.java
@@ -0,0 +1,28 @@
+package Study.dio.domain.model;
+
+public class News {
+	
+	private String icon;
+	private String description;
+	
+	
+	
+	public News() {
+		super();
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
