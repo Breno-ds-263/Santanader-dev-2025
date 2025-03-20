9ca1c6b18cc5ede3b8233372e447e1e26417a846
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-19 22:37:46 -0300

    initial commit

---
+++ src/main/java/Study/dio/SantanaderDev2025Application.java
@@ -0,0 +1,13 @@
+package Study.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanaderDev2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanaderDev2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/Study/dio/SantanaderDev2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanaderDev2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
