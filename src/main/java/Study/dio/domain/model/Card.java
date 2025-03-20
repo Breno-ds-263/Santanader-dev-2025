a34be1ac0dbe1bffeef72fd7f15757b9f1f2619b
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-20 16:41:35 -0300

    fix<Card>: add ORM

--- src/main/java/Study/dio/domain/model/Card.java
+++ src/main/java/Study/dio/domain/model/Card.java
@@ -1,12 +1,38 @@
 package Study.dio.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_Card")
 public class Card {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
 	private String number;
+	@Column(name = "availale_limit",scale = 13, precision = 2)
 	private Double limit;
 	
 	public Card() {
 		super();
 	}
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
 
 	public String getNumber() {
 		return number;
