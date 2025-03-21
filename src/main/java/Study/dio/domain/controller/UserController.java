f336283651ab87fd533410e786e877c0a58d8bb9
Breno do nasicmento <Breno.ds263@gmail.com>
2025-03-20 22:41:21 -0300

    Feat<UserController>: add controller for User

---
+++ src/main/java/Study/dio/domain/controller/UserController.java
@@ -0,0 +1,40 @@
+package Study.dio.domain.controller;
+
+import Study.dio.domain.model.User;
+import Study.dio.domain.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.create(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+}
+
